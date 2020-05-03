06c05198f09fa061ab08c3b31c7f1bd112fbbe07
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-02 17:31:53 -0700

    able to get keanu pics to randomize and render randomly to page

--- clicky_game/src/components/KeanuCard/index.js
+++ clicky_game/src/components/KeanuCard/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./style.css";
 
-function KeanuCard(props) {
+function randomizekeanuPics(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function KeanuCard({ keanuPics }) {
+  const randomKeanus = randomizekeanuPics(keanuPics);
   return (
     <div className="card-group">
-      {props.keanuPics.map((item) => (
+      {randomKeanus.map((item) => (
         <div className="img-container">
           <div className="card" key={item.id}>
             <img alt={item.name} src={item.image} />
