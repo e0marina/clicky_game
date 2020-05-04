f77954d5197f1d42d194ad8b24aebb5069601651
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-03 19:31:46 -0700

    still having trouble with onClick

--- src/components/KeanuCard/index.js
+++ src/components/KeanuCard/index.js
@@ -1,28 +1,37 @@
 import React from "react";
 import "./style.css";
 
-function randomizekeanuPics(array) {
-  let i = array.length - 1;
-  for (; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-}
+// function randomizekeanuPics(array) {
+//   let i = array.length - 1;
+//   for (; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     const temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//   }
+//   return array;
+// }
+// const randomKeanus = randomizekeanuPics(keanuPics);
+// {randomKeanus.map((item) => (
 
-function KeanuCard({ keanuPics }) {
-  const randomKeanus = randomizekeanuPics(keanuPics);
+//   <div className="card-group">
+
+//   <div className="img-container">
+//     <button>
+//       <img key={item.id} alt={item.name} src={item.image} />
+//     </button>
+//   </div>
+// ))}
+// </div>
+function KeanuCard(props) {
   return (
     <div className="card-group">
-      {randomKeanus.map((item) => (
-        <div className="img-container">
-          <button>
-            <img key={item.id} alt={item.name} src={item.image} />
-          </button>
-        </div>
-      ))}
+      <button
+        onClick={() => props.handleClick(props.id)}
+        className="clickHandler"
+      >
+        <img alt={props.name} src={props.image} />
+      </button>
     </div>
   );
 }
