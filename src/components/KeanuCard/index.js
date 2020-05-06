ba185e03d4a6146aa4972ab36cf1fdda5d4a24a5
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-05 21:50:55 -0700

    updated KeanuCard

--- src/components/KeanuCard/index.js
+++ src/components/KeanuCard/index.js
@@ -3,11 +3,20 @@ import "./style.css";
 
 function KeanuCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <div className="card-group">
-          <div onClick={() => props.clicked(props.id)} className="clickHandler">
-            <img alt={props.name} src={props.image} />
+    <div className="img-container">
+      <div className="row">
+        <div className="col-md">
+          <div className="card" width="18rem">
+            <div className="img-container">
+              <div className="card-group">
+                <div
+                  onClick={() => props.clicked(props.id)}
+                  className="clickHandler"
+                >
+                  <img alt={props.name} src={props.image} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
